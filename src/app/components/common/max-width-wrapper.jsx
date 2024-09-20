c2cab8b18bebe60f1f84c8100e5c6cfa2ef36f72
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2024-09-20 14:46:57 +0545

    input field

---
+++ src/app/components/common/max-width-wrapper.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+function MaxWidthWrapper({children}) {
+  return (
+    <div className="w-full max-w-[1260px] mx-auto px-5 md:px-3 xl:px-0">
+        {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
